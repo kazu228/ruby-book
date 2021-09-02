82830aa36146b0364c28d1f52cfbc5358835e324
kazu228 <kskkm0228@gmail.com>
2021-09-02 18:40:21 +0900

    fizz_buzzのテストコードをかいてみた。

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,20 +1,26 @@
 
 def fizz_buzz(n)
     if n % 15 == 0
-        puts'Fizz Buzz'
+        'Fizz Buzz'
     elsif n % 3 == 0
-        puts 'Fizz'
+        'Fizz'
     elsif n % 5 == 0
-        puts 'Buzz'
+        'Buzz'
     else
-        puts n.to_s
+        n.to_s
     end
 end
 
-fizz_buzz(1)
-fizz_buzz(2)
-fizz_buzz(3)
-fizz_buzz(4)
-fizz_buzz(5)
-fizz_buzz(6)
-fizz_buzz(15)
+require 'minitest/autorun'
+
+class FizzBuzzTest < Minitest::Test
+    def test_fizz_buzz
+        assert_equal '1', fizz_buzz(1)
+        assert_equal '2', fizz_buzz(2)
+        assert_equal 'Fizz', fizz_buzz(3)
+        assert_equal '4', fizz_buzz(4)
+        assert_equal 'Buzz', fizz_buzz(5)
+        assert_equal 'Fizz', fizz_buzz(6)
+        assert_equal 'Fizz Buzz', fizz_buzz(15)
+    end
+end
